2af1c8920ef7c2e2b0cb415daa5b718b387b0682
Fernando <rodriguez1fernando@outlook.com>
2023-05-02 22:25:59 -0600

    delete idgen

--- src/idgen.js
+++
@@ -1,7 +0,0 @@
-const caracteres =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-export let code = "";
-for (let i = 0; i < 20; i++) {
-  const randomIndex = Math.floor(Math.random() * caracteres.length);
-  code += caracteres[randomIndex];
-}
